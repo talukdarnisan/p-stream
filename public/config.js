3fc1c2c3c57f33044047b334795000d25e393a45
talukdarnisan <talukdarnisan@gmail.com>
2025-09-29 12:47:07 +0000

    updated by Google code assist

--- public/config.js
+++ public/config.js
@@ -1,23 +1,21 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-
-  https://simple-proxy.fawesome.workers.dev/?destination=
-  VITE_CORS_PROXY_URL: "https://simple-proxy.vmoviesfiles.workers.dev",
+  VITE_CORS_PROXY_URL: import.meta.env.VITE_CORS_PROXY_URL,
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "b708dcff37d65758ecc262914f19fd22",
+  VITE_TMDB_READ_API_KEY: import.meta.env.VITE_TMDB_READ_API_KEY,
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
-  VITE_DMCA_EMAIL: null,
+  VITE_DMCA_EMAIL: import.meta.env.VITE_DMCA_EMAIL === 'null' ? null : import.meta.env.VITE_DMCA_EMAIL,
 
   // Whether to disable hash-based routing, leave this as false if you don't know what this is
-  VITE_NORMAL_ROUTER: true,
+  VITE_NORMAL_ROUTER: import.meta.env.VITE_NORMAL_ROUTER === 'true',
 
   // The backend URL to communicate with
-  VITE_BACKEND_URL: "https://pstream-backend.onrender.com",
+  VITE_BACKEND_URL: import.meta.env.VITE_BACKEND_URL,
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
-  VITE_DISALLOWED_IDS: "",
-  VITE_APP_DOMAIN:"https://p-stream-sooty.vercel.app"
+  VITE_DISALLOWED_IDS: import.meta.env.VITE_DISALLOWED_IDS,
+  VITE_APP_DOMAIN: import.meta.env.VITE_APP_DOMAIN
 };
