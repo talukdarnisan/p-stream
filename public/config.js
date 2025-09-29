21bd1a86449bc58eaf7306fe105aaa0323d377fd
talukdarnisan <talukdarnisan@gmail.com>
2025-09-29 18:05:26 +0600

    Update config.js

--- public/config.js
+++ public/config.js
@@ -13,7 +13,7 @@ window.__CONFIG__ = {
   VITE_NORMAL_ROUTER: true,
 
   // The backend URL to communicate with
-  VITE_BACKEND_URL: "https://simple-proxy.fawesome.workers.dev/?destination=https://server.fifthwit.net",
+  VITE_BACKEND_URL: "https://simple-proxy.fawesome.workers.dev/?destination=https://glowing-lamp-v6g7wqg65q9436xgw-3000.app.github.dev",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: "",
